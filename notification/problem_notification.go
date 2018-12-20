a3ce379ca6fb0d770db293aa422fc22e17c7e664
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2018-12-20 23:55:27 +0100

    added ProblemDetailsText

--- notification/problem_notification.go
+++ notification/problem_notification.go
@@ -2,13 +2,14 @@ package notification
 
 // Default TODO: documentation
 type Default struct {
-	PID            string `json:"PID,omitempty"`
-	ProblemID      string `json:"ProblemID,omitempty"`
-	State          string `json:"State,omitempty"`
-	Title          string `json:"ProblemTitle,omitempty"`
-	URL            string `json:"ProblemURL,omitempty"`
-	Impact         string `json:"ProblemImpact,omitempty"`
-	ImpactedEntity string `json:"ImpactedEntity,omitempty"`
-	Severity       string `json:"ProblemSeverity,omitempty"`
-	Tags           string `json:"Tags,omitempty"`
+	PID                string `json:"PID,omitempty"`
+	ProblemID          string `json:"ProblemID,omitempty"`
+	State              string `json:"State,omitempty"`
+	Title              string `json:"ProblemTitle,omitempty"`
+	URL                string `json:"ProblemURL,omitempty"`
+	Impact             string `json:"ProblemImpact,omitempty"`
+	ImpactedEntity     string `json:"ImpactedEntity,omitempty"`
+	Severity           string `json:"ProblemSeverity,omitempty"`
+	Tags               string `json:"Tags,omitempty"`
+	ProblemDetailsText string `json:"ProblemDetailsText,omitempty"`
 }
