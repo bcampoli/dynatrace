aad7f3b51e0f087f4ec3395834753f9040040698
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2019-02-11 12:35:28 +0100

    fix: json annotation for name attribute

--- apis/onprem/user_groups/group_config.go
+++ apis/onprem/user_groups/group_config.go
@@ -3,7 +3,7 @@ package usersgroups
 // GroupConfig TODO: documentation
 type GroupConfig struct {
 	ID                  string              `json:"id"`                  // Group ID
-	Name                string              `name:"name"`                // Group name
+	Name                string              `json:"name"`                // Group name
 	LDAPGroupNames      []string            `json:"ldapGroupNames"`      // LDAP group names
 	AccessRight         map[string][]string `json:"accessRight"`         // Access rights
 	IsClusterAdminGroup bool                `json:"isClusterAdminGroup"` // If true, then the cluster has administrator rights
