1af04819b9ce0c4e5a765e3f65cae16ee7a52ec8
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2019-02-11 12:37:31 +0100

    added omitempty to ID property

--- apis/onprem/user_groups/group_config.go
+++ apis/onprem/user_groups/group_config.go
@@ -2,7 +2,7 @@ package usersgroups
 
 // GroupConfig TODO: documentation
 type GroupConfig struct {
-	ID                  string              `json:"id"`                  // Group ID
+	ID                  string              `json:"id,omitempty"`        // Group ID
 	Name                string              `json:"name"`                // Group name
 	LDAPGroupNames      []string            `json:"ldapGroupNames"`      // LDAP group names
 	AccessRight         map[string][]string `json:"accessRight"`         // Access rights
